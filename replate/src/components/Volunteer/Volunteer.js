ee6a908e6260ae4229eb93d517cd532fe58dfe65
raberin <rmvaberin@gmail.com>
2019-04-24 07:59:07 -0700

    Added return() in Volunteer.js on the render function

--- replate/src/components/Volunteer/Volunteer.js
+++ replate/src/components/Volunteer/Volunteer.js
@@ -27,70 +27,72 @@ class Volunteer extends React.Component {
   };
 
   render() {
-    <div className="sign-up-wrapper">
-      <div className="sign-up-title">
-        <h2>Volunteer Sign Up</h2>
-        <p>Thanks for your interest!</p>
+    return (
+      <div className="sign-up-wrapper">
+        <div className="sign-up-title">
+          <h2>Volunteer Sign Up</h2>
+          <p>Thanks for your interest!</p>
+        </div>
+        <form className="sign-up-form">
+          <h2>Create Your Account</h2>
+          <div className="input-field">
+            <p>First Name</p>
+            <input
+              type="text"
+              name="firstName"
+              onchange={this.changeHandler}
+              value={this.state.newVolunteer.name}
+            />
+          </div>
+          <div className="input-field">
+            <p>Last Name</p>
+            <input
+              type="text"
+              name="lastName"
+              onchange={this.changeHandler}
+              value={this.state.newVolunteer.name}
+            />
+          </div>
+          <div className="input-field">
+            <p>Phone</p>
+            <input
+              type="text"
+              name="phone"
+              onchange={this.changeHandler}
+              value={this.state.newVolunteer.name}
+            />
+          </div>
+          <div className="input-field">
+            <p>E-mail</p>
+            <input
+              type="text"
+              name="email"
+              onchange={this.changeHandler}
+              value={this.state.newVolunteer.name}
+            />
+          </div>
+          <div className="input-field">
+            <p>Password</p>
+            <input
+              type="password"
+              name="password"
+              onchange={this.changeHandler}
+              value={this.state.newVolunteer.name}
+            />
+          </div>
+          <div className="input-field">
+            <p>Repeat password</p>
+            <input
+              type="password"
+              name="repeatPassword"
+              onchange={this.changeHandler}
+              value={this.state.newVolunteer.name}
+            />
+          </div>
+          <button type="submit">Continue</button>
+        </form>
       </div>
-      <form className="sign-up-form">
-        <h2>Create Your Account</h2>
-        <div className="input-field">
-          <p>First Name</p>
-          <input
-            type="text"
-            name="firstName"
-            onchange={this.changeHandler}
-            value={this.state.newVolunteer.name}
-          />
-        </div>
-        <div className="input-field">
-          <p>Last Name</p>
-          <input
-            type="text"
-            name="lastName"
-            onchange={this.changeHandler}
-            value={this.state.newVolunteer.name}
-          />
-        </div>
-        <div className="input-field">
-          <p>Phone</p>
-          <input
-            type="text"
-            name="phone"
-            onchange={this.changeHandler}
-            value={this.state.newVolunteer.name}
-          />
-        </div>
-        <div className="input-field">
-          <p>E-mail</p>
-          <input
-            type="text"
-            name="email"
-            onchange={this.changeHandler}
-            value={this.state.newVolunteer.name}
-          />
-        </div>
-        <div className="input-field">
-          <p>Password</p>
-          <input
-            type="password"
-            name="password"
-            onchange={this.changeHandler}
-            value={this.state.newVolunteer.name}
-          />
-        </div>
-        <div className="input-field">
-          <p>Repeat password</p>
-          <input
-            type="password"
-            name="repeatPassword"
-            onchange={this.changeHandler}
-            value={this.state.newVolunteer.name}
-          />
-        </div>
-        <button type="submit">Continue</button>
-      </form>
-    </div>;
+    );
   }
 }
 
