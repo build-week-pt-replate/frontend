cff52f400882378cd713d339dae75e7eb91eb6de
raberin <rmvaberin@gmail.com>
2019-04-24 19:07:34 -0700

    Added routes and links to signup pages

--- replate/src/App.js
+++ replate/src/App.js
@@ -1,14 +1,19 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Route, Link } from "react-router-dom";
+
+import SignUp from "./components/SignUp/SignUp";
+import Volunteer from "./components/Volunteer/Volunteer";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Replate
-        </p>
-
+        <p>Replate</p>
+        <Link to="/login">Login</Link>
+        <Link to="/signup">Sign Up</Link>
+        <Route path="/signup" component={SignUp} />
+        <Route path="/signup/volunteer" component={Volunteer} />
       </header>
     </div>
   );
