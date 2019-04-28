16e546929a62d7e6346151df6e50d6cccaf1d876
raberin <rmvaberin@gmail.com>
2019-04-27 20:14:44 -0700

    Added Matrial-UI, topBar

--- replate/src/App.js
+++ replate/src/App.js
@@ -4,6 +4,7 @@ import { Route, Link } from "react-router-dom";
 
 import SignUp from "./components/SignUp/SignUp";
 import Login from "./components/Login/Login";
+import SignIn from "./components/Login/SignIn";
 // import PrivateRoute from "./components/PrivateRoute";
 import VolunteerDash from "./components/Volunteer/VolunteerDash";
 import VolunteerForm from "./components/Volunteer/VolunteerForm";
@@ -15,10 +16,12 @@ function App() {
       <header className="App-header">
         <p>Replate</p>
         <Link to="/login">Login</Link>
+        <Link to="/login2">Login V2</Link>
         <Link to="/signup">Sign Up</Link>
 
         <Route exact path="/signup/" component={SignUp} />
         <Route path="/login" component={Login} />
+        <Route path="/login2" component={SignIn} />
         <Route path="/signup/volunteer" component={VolunteerForm} />
         <Route path="/signup/business" component={SignUpFormContainer} />
         <Route path="/volunteer/dashboard" component={VolunteerDash} />
--- replate/src/components/Header/Header.jsx
+++ replate/src/components/Header/Header.jsx
@@ -1,12 +1,49 @@
 import React from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import { Link } from "react-router-dom";
 
-const Header = () => {
-  return (
-    <header>
-      <h1>Replate</h1>
-      <ul>li*</ul>
-    </header>
-  );
+const styles = {
+  root: {
+    flexGrow: 1
+  },
+  grow: {
+    flexGrow: 1
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20
+  }
 };
 
-export default Header;
+function ButtonAppBar(props) {
+  const { classes } = props;
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" color="inherit" className={classes.grow}>
+            Replate
+          </Typography>
+          <Button color="inherit">
+            <Link className="link-router" to="/login">
+              Login
+            </Link>
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
+
+ButtonAppBar.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(ButtonAppBar);
