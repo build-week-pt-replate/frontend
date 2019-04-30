e79e5a58f65c4082c47326211a8d3678a31fdb82
LD <LDinho@users.noreply.github.com>
2019-04-29 20:40:04 -0400

    [business]-set up initial dasboard; update sign up form field names; & update App component Routes
    - attempt to post mocking data from createBusinessAccount() in actions
    - In <App> remove <Route > from inside of <header> tag and place outside of it
    - set up initial BusinessDash component
    - update business sign up form fields property names to better match BE

---
+++ replate/src/components/Business/BusinessDash.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+class BusinessDash extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+
+  }
+
+  render() {
+    return (
+      <div className="business-dash">
+        <div className=" ">
+          <h3>Business</h3>
+        </div>
+        <div className=" ">
+          <h3>Schedule</h3>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default BusinessDash;
