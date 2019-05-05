c2c81fa95da26bbc06657d60b416d0c0dc659357
raberin <rmvaberin@gmail.com>
2019-05-04 19:43:23 -0700

    Added add requests and remove request functionality with mock data - not functional online as of yet

--- replate/src/components/Volunteer/RequestList.jsx
+++ replate/src/components/Volunteer/RequestList.jsx
@@ -1,23 +1,34 @@
-import React from 'react'
-import RequestCard from './RequestCard';
+import React from "react";
+import RequestCard from "./RequestCard";
 
-import './RequestList.css'
+import "./RequestList.css";
 
 const RequestList = props => {
   return (
-    <div className='available-requests-container'>
+    <div className="available-requests-container">
       {props.requests
         .filter(request => {
           //Checks if the requests is in volunteer city and has not been taken
-          if (props.account.city === request.locationCity && request.volunteerId === null) {
-            return request
+          if (
+            props.account.city === request.locationCity &&
+            request.volunteerId === null
+          ) {
+            return request;
           }
         })
         .map(request => {
-          return <RequestCard request={request} key={request.id} account={props.account} updatedRequest={props.updatedRequest} saveRequest={props.saveRequest} />
+          return (
+            <RequestCard
+              request={request}
+              key={request.id}
+              account={props.account}
+              acceptRequest={props.acceptRequest}
+              removeRequest={props.removeRequest}
+            />
+          );
         })}
     </div>
-  )
-}
+  );
+};
 
-export default RequestList
+export default RequestList;
--- replate/src/components/Volunteer/VolunteerDash.jsx
+++ replate/src/components/Volunteer/VolunteerDash.jsx
@@ -3,12 +3,12 @@ import DashHeader from "../Header/DashHeader";
 import { connect } from "react-redux";
 import {
   fetchVolunteerRequests,
-  fetchVolunteerData,
+  fetchVolunteerData
   // acceptVolunteerRequest
-} from '../../actions';
+} from "../../actions";
 
-import RequestList from './RequestList'
-import VolunteerRequestList from './VolunteerRequestList'
+import RequestList from "./RequestList";
+import VolunteerRequestList from "./VolunteerRequestList";
 
 class VolunteerDash extends React.Component {
   constructor(props) {
@@ -16,74 +16,70 @@ class VolunteerDash extends React.Component {
     this.state = {
       account: this.props.account,
       updatedRequest: null
-    }
-
+    };
   }
 
   componentDidMount() {
     //Fetch volunteer data
-    this.props.fetchVolunteerData(localStorage.getItem('id'))
+    this.props.fetchVolunteerData(localStorage.getItem("id"));
     // fetch all requests
     // this.props.fetchVolunteerRequests();
   }
 
-  saveRequest = request => {
+  acceptRequest = request => {
+    console.log(request);
+    let newRequest = request;
+    console.log(newRequest);
+    newRequest.volunteerId = localStorage.getItem("id");
     this.setState({
-      updatedRequest: {
-        request
-      }
-    })
-    console.log(this.state.updatedRequest)
-  }
+      updatedRequest: newRequest
+    });
+    console.log("After first set", this.state.updatedRequest);
+  };
 
-  // saveRequest = e => {
-  //   //Changes the state/tracks the value of the friendPost whatever is being inputed on the form
-  //   let values = e.target.value
-  //   this.setState({
-  //     updatedRequest: {
-  //       ...this.state.updatedRequest,
-  //       //Since on the inputs theres a "name" value it can be used as a key to change
-  //       //All the inputs dynamically instead of creating a handler for all of em
-  //       values
-  //     }
-  //   });
-  //   console.log(this.state.updatedRequest)
-  // };
-
-  // acceptRequest = () => {
-  //   let updatedRequest = {
-  //     ...this.state.upd
-  //   }
-
-
-
-  //   acceptVolunteerRequest(updatedRequest)
-  // }
-
+  removeRequest = request => {
+    console.log(request);
+    let newRequest = request;
+    console.log(newRequest);
+    newRequest.volunteerId = null;
+    this.setState({
+      updatedRequest: newRequest
+    });
+    console.log("After first set", this.state.updatedRequest);
+  };
 
   render() {
     return (
       <div className="dash-container">
-      {this.props.account ? (
-        <div>
-          <DashHeader history={this.props.history} />
-          <div className="volunteer-dash">
-            <h2>{this.props.account.firstName}'s Dashboard</h2>
-            {/* <h2>Volunteer Dashboard</h2> */}
-            <h3>City:{this.props.account.city}</h3>
-            <div className="requests-box">
-              <h3>Available Requests</h3>
-              <RequestList requests={this.props.requests} account={this.props.account} updatedRequest={this.updatedRequest} saveRequest={this.saveRequest} />
-            </div>
-            <div className="requests-box">
-              <h3>Current Requests</h3>
-              <VolunteerRequestList requests={this.props.requests} account={this.props.account} />
+        {this.props.account ? (
+          <div>
+            <DashHeader history={this.props.history} />
+            <div className="volunteer-dash">
+              <h2>{this.props.account.firstName}'s Dashboard</h2>
+              {/* <h2>Volunteer Dashboard</h2> */}
+              <h3>City:{this.props.account.city}</h3>
+              <div className="requests-box">
+                <h3>Available Requests</h3>
+                <RequestList
+                  requests={this.props.requests}
+                  account={this.props.account}
+                  acceptRequest={this.acceptRequest}
+                  removeRequest={this.removeRequest}
+                />
+              </div>
+              <div className="requests-box">
+                <h3>Current Requests</h3>
+                <VolunteerRequestList
+                  requests={this.props.requests}
+                  account={this.props.account}
+                  removeRequest={this.removeRequest}
+                />
+              </div>
             </div>
           </div>
-        </div>
-      ) : (
-        <h3>Loading</h3>
-      )}
+        ) : (
+          <h3>Loading</h3>
+        )}
       </div>
     );
   }
@@ -99,4 +95,7 @@ const mapStateToProps = state => {
   };
 };
 
-export default connect(mapStateToProps, { fetchVolunteerRequests, fetchVolunteerData })(VolunteerDash)
+export default connect(
+  mapStateToProps,
+  { fetchVolunteerRequests, fetchVolunteerData }
+)(VolunteerDash);
