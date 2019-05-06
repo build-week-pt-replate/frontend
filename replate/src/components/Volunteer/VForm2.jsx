445e5d20dfbf6bfb9e02eefe74a17c99607e6030
raberin <rmvaberin@gmail.com>
2019-05-05 21:16:52 -0700

    Styled Signup Form, Got accept and remove requests on volunteer to work, Complete requests still not functioining

---
+++ replate/src/components/Volunteer/VForm2.jsx
@@ -0,0 +1,183 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import FormControl from "@material-ui/core/FormControl";
+// import FormControlLabel from "@material-ui/core/FormControlLabel";
+// import Checkbox from "@material-ui/core/Checkbox";
+import Input from "@material-ui/core/Input";
+import InputLabel from "@material-ui/core/InputLabel";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+import withStyles from "@material-ui/core/styles/withStyles";
+// import Switch from "@material-ui/core/Switch";
+
+const styles = theme => ({
+  main: {
+    width: "auto",
+    display: "block", // Fix IE 11 issue.
+    marginLeft: theme.spacing.unit * 3,
+    marginRight: theme.spacing.unit * 3,
+    [theme.breakpoints.up(400 + theme.spacing.unit * 3 * 2)]: {
+      width: 400,
+      marginLeft: "auto",
+      marginRight: "auto"
+    }
+  },
+  paper: {
+    marginTop: theme.spacing.unit * 8,
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    padding: `${theme.spacing.unit * 2}px ${theme.spacing.unit * 3}px ${theme
+      .spacing.unit * 3}px`
+  },
+  avatar: {
+    margin: theme.spacing.unit,
+    backgroundColor: theme.palette.secondary.main
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing.unit
+  },
+  submit: {
+    marginTop: theme.spacing.unit * 3
+  }
+});
+
+function VForm2(props) {
+  const { classes } = props;
+
+  return (
+    <main className={classes.main}>
+      <CssBaseline />
+      <Paper className={classes.paper}>
+
+        <Typography component="h1" variant="h5">
+          <strong>Volunteer Signup</strong>
+        </Typography>
+        <form className={classes.form} onSubmit={props.handleLogin}>
+          <FormControl margin="normal" required fullWidth>
+            <InputLabel htmlFor="email">Email Address</InputLabel>
+            <Input
+              id="email"
+              name="email"
+              autoComplete="email"
+              autoFocus
+              onChange={props.changeHandler}
+            />
+          </FormControl>
+
+          <FormControl margin="normal" required fullWidth>
+            <InputLabel htmlFor="password">Password</InputLabel>
+            <Input
+              name="password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              onChange={props.changeHandler}
+            />
+          </FormControl>
+
+          <FormControl margin="normal" required fullWidth>
+            <InputLabel htmlFor="repeatPassword">Repeat Password</InputLabel>
+            <Input
+              name="repeatPassword"
+              type="repeatPassword"
+              id="repeatPassword"
+              autoComplete="current-password"
+              onChange={props.changeHandler}
+            />
+          </FormControl>
+
+          <FormControl margin="normal" required fullWidth>
+            <InputLabel htmlFor="firstName">First Name</InputLabel>
+            <Input
+              id="firstName"
+              name="firstName"
+              autoComplete="firstName"
+              autoFocus
+              onChange={props.changeHandler}
+            />
+          </FormControl>
+
+          <FormControl margin="normal" required fullWidth>
+            <InputLabel htmlFor="lastName">Last Name</InputLabel>
+            <Input
+              id="lastName"
+              name="lastName"
+              autoComplete="lastName"
+              autoFocus
+              onChange={props.changeHandler}
+            />
+          </FormControl>
+
+
+
+          <FormControl margin="normal" required fullWidth>
+            <InputLabel htmlFor="phone">Phone</InputLabel>
+            <Input
+              id="phone"
+              name="phone"
+              autoComplete="phone"
+              autoFocus
+              onChange={props.changeHandler}
+            />
+          </FormControl>
+
+          <FormControl margin="normal" required fullWidth>
+            <InputLabel htmlFor="city">City</InputLabel>
+            <Input
+              id="city"
+              name="city"
+              autoComplete="city"
+              autoFocus
+              onChange={props.changeHandler}
+            />
+          </FormControl>
+
+          <FormControl margin="normal" required fullWidth>
+            <InputLabel htmlFor="state">State</InputLabel>
+            <Input
+              id="state"
+              name="state"
+              autoComplete="state"
+              autoFocus
+              onChange={props.changeHandler}
+            />
+          </FormControl>
+
+          <FormControl margin="normal" required fullWidth>
+            <InputLabel htmlFor="zip">Zipcode</InputLabel>
+            <Input
+              id="zip"
+              name="zip"
+              autoComplete="zip"
+              autoFocus
+              onChange={props.changeHandler}
+            />
+          </FormControl>
+
+
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Sign in
+          </Button>
+        </form>
+      </Paper>
+    </main>
+  );
+}
+
+// LoginForm.propTypes = {
+//   classes: PropTypes.object.isRequired
+// };
+
+export default withStyles(styles)(VForm2);
